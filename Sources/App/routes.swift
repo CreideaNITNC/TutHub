b127314f79c83388b791d2b775049f3f9a5f1036
tanaka <lemo.nade.engineer.room@gmail.com>
2022-12-02 20:15:27 +0900

    CDの完成

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,8 +5,4 @@ func routes(_ app: Application) throws {
     app.post("tanaka", "vapor") { req in
         HTTPStatus.created
     }
-
-    app.get("delivery") { req in
-        "delivery"
-    }
 }
