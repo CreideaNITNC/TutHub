d2b075db0103124588ad65f01418598bdfb9ac32
tanaka <i10566@nara.kosen-ac.jp>
2022-12-16 01:55:09 +0900

    並列化

--- Sources/App/Database/DatabaseTutPushRepository.swift
+++ Sources/App/Database/DatabaseTutPushRepository.swift
@@ -18,6 +18,45 @@ struct DatabaseTutPushRepository: TutPushRepository {
             }
             try await repository.$tags.create(tags, on: transaction)
             
+            await withThrowingTaskGroup(of: Void.self, returning: Void.self) { tagGroup in
+                for i in 0 ..< tags.count {
+                    tagGroup.addTask {
+                        let tagModel = tags[i]
+                        let commits: [CommitModel] = try data.tags[i].commits.enumerated().map { (index, commit) in
+                                .init(step: index + 1, message: commit.message, tagID: try tagModel.requireID())
+                        }
+                        try await tagModel.$commits.create(commits, on: transaction)
+                        
+                        await withThrowingTaskGroup(of: Void.self) { commitGroup in
+                            for j in 0 ..< commits.count {
+                                commitGroup.addTask {
+                                    let commitModel = commits[j]
+                                    let files: (pictures: [PictureModel], codes: [SourceCodeModel]) = try data.tags[i].commits[j].files
+                                        .reduce((pictures: [], codes: [])) { (all, file) in
+                                            if file.type == .text {
+                                                return (pictures: all.pictures, codes: all.codes + [.init(
+                                                    filename: file.name,
+                                                    code: file.content,
+                                                    commitID: try commitModel.requireID()
+                                                )])
+                                            } else {
+                                                return (pictures: all.pictures + [.init(
+                                                    filename: file.name,
+                                                    bin: file.content,
+                                                    commitID: try commitModel.requireID()
+                                                )], codes: all.codes)
+                                            }
+                                        }
+                                    async let createPictures: () = commitModel.$pictures.create(files.pictures, on: transaction)
+                                    async let createCodes: () = commitModel.$codes.create(files.codes, on: transaction)
+                                    _ = try await [createPictures, createCodes]
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
             for i in 0 ..< tags.count {
                 let tagModel = tags[i]
                 let commits: [CommitModel] = try data.tags[i].commits.enumerated().map { (index, commit) in
