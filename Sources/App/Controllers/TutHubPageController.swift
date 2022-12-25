4a6d869aa0949fee003f1ae2b744a97506c360b0
TanakaYouichirou <lemo.nade.engineer.room@gmail.com>
2022-12-26 02:07:00 +0900

    テストを修正

--- Sources/App/Controllers/TutHubPageController.swift
+++ Sources/App/Controllers/TutHubPageController.swift
@@ -9,7 +9,9 @@ struct TutHubPageController: RouteCollection {
     func index(req: Request) async throws -> TutorialPageData {
         let username = try Username(req.parameters.get("username")!)
         let repositoryName = try RepositoryName(req.parameters.get("repository")!)
-        let page = try SectionPage(req.parameters.get("page", as: Int.self)!)
+        guard let page = try req.parameters.get("page", as: Int.self).map({ try SectionPage($0) }) else {
+            throw Abort(.badRequest, reason: "ページ番号は数値である必要があります")
+        }
         
         let content = try await req.tutHubPageService.read(username, repositoryName, page)
         return .init(username, repositoryName, page, content)
--- Tests/UnitTests/Entities/User/UserValues/MailAddressTest.swift
+++ Tests/UnitTests/Entities/User/UserValues/MailAddressTest.swift
@@ -4,7 +4,7 @@ import XCTVapor
 final class MailAddressTest: XCTestCase {
 
     func test_正常値() throws {
-        XCTAssertThrowsError(try MailAddress("test@example.com"))
+        _ = try MailAddress("test@example.com")
     }
     
     func test_不正値1文字() throws {
